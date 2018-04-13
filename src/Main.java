6a5ddd96c9a1478069e3af56d632c9bd24e02151
liujianbin <liujianbin@qianxx.com>
2018-04-13 11:26:37 +0800

    Byte数据类型重解。

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import Map.MapTest;
+import dataType.ByteType;
 import dataType.IntegerType;
+import math.BitMath;
 import object.ObjectTest;
 /**
  * Description 主函数入口
  */
 public class Main {
     public static void main(String[] args) {
-        IntegerType.test();
+        ByteType.test();
+
     }
 }
--- src/dataType/ByteType.java
+++ src/dataType/ByteType.java
@@ -2,89 +2,41 @@ package dataType;
 
 /**
  * Created by knbsyoo on 2018/2/28
- * Description: 熟悉Byte数据类型及
+ * 解析笔记：https://blog.csdn.net/qq_21397851/article/details/79900658
  */
 public class ByteType {
     public static void test(){
-        //Byte8位，第一位为符号位。
-        Byte max = Byte.MAX_VALUE;//01111111-> 127
-        out("Byte最大值：" + max);
-        Byte mix = Byte.MIN_VALUE;//11111111-> -128 负数使用补码表示，在原码的基础上（不包含符号位）取反加1
-        out("Byte最小值：" + mix);
-        Class<Byte> type = Byte.TYPE;
-        out("Byte对应的基础数据类型：" + type);
-        int size = Byte.SIZE;
-        out("Byte长度：" + size);
+        //Byte声明
+        byte byte1 = 1;
+        Byte b1 = new Byte(byte1);
+        Byte b2 = new Byte("1");
+        Byte b3 = byte1;//自动装箱
+        out(b1==b2);//fasle
+        out(b1==b3);//fasle
+        //初始化方法，除构造方法和自动装箱
+        Byte b4 = Byte.parseByte("1",10);//最终byte自动装箱
+        Byte b5 = Byte.parseByte("1");//同上
+        Byte b6 = Byte.valueOf("1",10);//最终取ByteCache
+        Byte b7 = Byte.valueOf("1");//同上
+        Byte b8 = Byte.valueOf(byte1);//同上
+        out(b4==b2);//fasle
+        out(b4==b1);//fasle
+        out(b4==b3);//true
+        out(b4==b5);//true
+        out(b6==b7);//true
+        out(b6==b8);//true
 
-        //类型转换
-        String maxStr = max.toString();
-        out("maxStr=" + maxStr );
-        byte byte1 = -1;
-        //valueOf()方法调用ByteCache 内部私有类，初始化Byte数组，范围-128-127，可以直接从类内部获取Byte对象。
-        Byte test1  = Byte.valueOf(byte1);
-        out("test1：" + test1);
-        try {
-            Byte test2 = Byte.parseByte("123");//parseByte()返回byte类型，byte自动装箱转换为Byte。
-            out("test2：" + test2);
-        }catch (NumberFormatException e){
-            out("get a NumberFormatException at line 28");
-        }
-        try {
-            Byte test3 = Byte.parseByte("1231");
-            out("test3：" + test3);
-        }catch (NumberFormatException e){
-            out("get a NumberFormatException at line 34");
-        }
-        try {
-            Byte test3 = Byte.parseByte("11",2);
-            out("test3：" + test3);
-        }catch (NumberFormatException e){
-            out("get a NumberFormatException at line 40");
-        }
-        try {
-            Byte test3 = Byte.parseByte("1231");
-            out("test3：" + test3);
-        }catch (NumberFormatException e){
-            out("get a NumberFormatException at line 46");
-        }
-
-        //8,10,16进制解码转换为Byte，调用Integer的decode()
-        try {
-            Byte test4 = Byte.decode("51");//10进制
-            out("10进制：" + test4);
-            Byte test5 = Byte.decode("-51");//10进制
-            out("10进制：" + test5);
-            Byte test6 = Byte.decode("063");//8进制以0开头
-            out("8进制：" + test6);
-            Byte test7 = Byte.decode("-063");//待符号的8进制，根据实现的代码逻辑，可以带负号
-            out("8进制：" + test7);
-            Byte test8 = Byte.decode("0x33");//16进制，以0X或者0x开头
-            out("16进制：" + test8);
-            Byte test9 = Byte.decode("-0x33");
-            out("16进制：" + test9);
-        }catch (NumberFormatException e){
-            out("get a NumberFormatException at line 52 to line 63");
-        }
-
-        //显示转换 低-》高
-        byte test10 = 5;
-        Byte test11 = Byte.valueOf(test10);
-        short test12 = test11.shortValue();
-        int test13 = test11.intValue();
-        long test14 = test11.longValue();
-        float test15 = test11.floatValue();
-        double test16 = test11.doubleValue();
-
-        //hashCode
-        int hashCode = test11.hashCode();
-        out("hashCode：" + hashCode);
-
-        //比较 实现Comparable接口，重写了compareTo()
-        byte test17 = 7;
-        byte test18 = 4;
-        out(test11.equals(test10));//自动装箱
-        out(test11.compareTo(test17));
-        out(test11.compareTo(test18));
+        //其他方法
+        Byte.toString(byte1);//1
+        b8.toString();//1
+        b8.hashCode();//1
+        Byte.decode("0x8");//8
+        b4.equals(b2);//true
+        byte byte2 = -1;
+        Byte.toUnsignedInt(byte1);//1
+        Byte.toUnsignedLong(byte1);//1
+        Byte.toUnsignedInt(byte2);//255 强转为int的-1，-1的二进制补码：1111,1111,1111,1111,1111,1111,1111,1111，进行& 0xff，截取byte表示8位以外的其他高位。
+        Byte.toUnsignedLong(byte2);//255
     }
 
     private static void out(Object o){
